8a86ae083284a5c6e86c2b49c1810fac3f1f767a
sashaxodunov <sasha@xodunov.ru>
2026-05-09 09:11:45 +0300

    Урок 20. Усложненное задание

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import timer from "./module/timer";
 import menu from "./module/menu";
 import modal from "./module/modal";
+import smoothScroll from "./module/smoothScroll";
 
 timer("20 may 2026");
 menu()
 modal()
-
+smoothScroll()
